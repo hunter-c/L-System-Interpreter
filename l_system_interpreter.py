784f71286eafbc4f6b31c721cc5d9b992d6eb94d
Hunter Christenson <huntercc@berkeley.edu>
2022-02-02 01:01:48 -0800

    added f op (no draw) and a few test systems

--- l_system_interpreter.py
+++ l_system_interpreter.py
@@ -5,12 +5,13 @@ import random
 
 # L-system rules from http://paulbourke.net/fractals/lsys/
 
+
 def iterate(axiom, rules, num_iterations=3):
     constants = ['+', '-', '[', ']', '>']
 
     # creates new iteration by replacing current iters symbols based on rules
     def new_iteration(current):
-        iteration = '' # clear old rules to build new iteration
+        iteration = ''  # clear old rules to build new iteration
         for char in current:
             if char in constants:
                 iteration += char
@@ -24,21 +25,25 @@ def iterate(axiom, rules, num_iterations=3):
         print(f"Iteration: {i + 1} \n {iteration}")
     return iteration
 
+
 def draw(iteration, distance=10, angle=30, length_factor=1):
     stack = []
     cursor = turtle.Turtle()
-    #cursor.hideturtle()
-    # turtle.tracer(0, 0)
+    # cursor.hideturtle()
+    turtle.tracer(0, 0)
     cursor.speed(0)
     cursor.left(90)
     cursor.left(90)
 
-    turtle.setup(width = 1700, height = 900, startx = None, starty = None)
-
+    turtle.setup(width=1700, height=900, startx=None, starty=None)
 
     for op in iteration:
         if op == "F":
             cursor.forward(distance)
+        elif op == "f":
+            cursor.penup()
+            cursor.forward(distance)
+            cursor.pendown()
         elif op == "+":
             cursor.right(angle)
         elif op == "-":
@@ -55,12 +60,13 @@ def draw(iteration, distance=10, angle=30, length_factor=1):
             distance = distance * length_factor
         elif op == "<":
             distance = int(distance / length_factor)
-    #turtle.update()
+    # turtle.update()
+
 
 def generate_system():
     angles = [15, 30, 45, 60, 70, 90, 120, 135, 270]
     constants = ['F', '+', '-', '[', ']', '>', '<']
-    
+
     two_rule = bool(random.randrange(2))
     # if random.randrange(10) % 2 == 0:
     #     pass
@@ -70,8 +76,7 @@ def generate_system():
     size = random.randrange(4, 10)
 
     # for i in range(size // 2):
-    #     axiom += 
-
+    #     axiom +=
 
     # for i in range(rule_size):
 
@@ -91,12 +96,20 @@ def main():
     # draw(iterate("FXF", {"F":"+FXF+", "X":"FF"}, 7), 30, 30) # geometric lotus shape
     # draw(iterate("YF", { "G":"F+G+F"}, 10), 10, 60) # sierpinsky triangle
     # draw(iterate("YF", {"F":"F", "X":"YF+XF+Y", "Y":"XF-YF-X"}, 3), 10, 60) # sierpinsky triangle
-    # draw(iterate("a", {"F": ">F<", "a": "F[+x]Fb", "b": "F[-y]Fa", "x": "a", "y": "b"}, 9), 1, 45, 1.2) # leaf sort of
-    draw(iterate("a", {"F": "+F[a]F+F[a]F---FaF+FaF+", "a": "-FFF-"}, 5), 3, 12) # leaf sort of
+    # draw(iterate("a", {"F": ">F<", "a": "F[+x]Fb", "b": "F[-y]Fa", "x": "a", "y": "b"}, 9)
 
-    # irritating... well, heh... ehm, just an annoying continuation of the last few monthes, of which, I have had the unfortunate opportunity of experiencing in their entirety.
+    # Koch curve
+    # draw(iterate("F-F-F-F", {"F": "F+FF-FF-F-F+F+FF-F-F+F+FF+FF-F"}, 3), 2, 90)
+
+    # islands
+    # draw(iterate("F+F+F+F",
+    #      {"F": "F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF", "f": "ffffff"}, 2), 5, 90)
+
+    # Crystal
+    draw(iterate("F+F+F+F", {"F": "FF+F++F+F"}, 4), 5, 90)
 
     turtle.done()
+
 
 if __name__ == "__main__":
     main()
